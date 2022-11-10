490d09c1ac5c318880a204bf78347b0c239943f0
koudai.hirose <koudai.hirose@osaka-sanitary.co.jp>
2022-11-10 07:11:56 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -49,11 +49,13 @@ class TasksController extends Controller
     {
         // バリデーション
         $request->validate([
+            'status' => 'required|max:10', 
             'content' => 'required|max:255',
         ]);
         
         // タスクを作成
         $task = new Task;
+        $task->status = $request->status;    // 追加
         $task->content = $request->content;
         $task->save();
 
@@ -106,12 +108,14 @@ class TasksController extends Controller
     {
         // バリデーション
         $request->validate([
+            'status' => 'required|max:10',  
             'content' => 'required|max:255',
         ]);
        
         // idの値でタスクを検索して取得
         $task = Task::findOrFail($id);
         // タスクを更新
+        $task->status = $request->status;    // 追加
         $task->content = $request->content;
         $task->save();
 
